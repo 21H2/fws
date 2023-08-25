33c7f058e1d32b7b10768ab707b8b244dffddfe1
Umesh Sharma <83266784+21H2@users.noreply.github.com>
2023-08-26 00:30:01 +0530

    script.js

--- script.js
+++ script.js
@@ -30,3 +30,48 @@ function updateVoteCounts() {
 
 // Initial update of vote counts
 updateVoteCounts();
+
+// Your existing code...
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDSEgQW3AVCyq2GEK7ySOwiIo7QGdcJkx0",
+    authDomain: "voting-ac3b3.firebaseapp.com",
+    projectId: "voting-ac3b3",
+    storageBucket: "voting-ac3b3.appspot.com",
+    messagingSenderId: "1058093814912",
+    appId: "1:1058093814912:web:0177e3035d91e88f05c265",
+    measurementId: "G-X20NQNRWVT"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Reference to the Firebase database
+const database = firebase.database();
+
+// Get a reference to the "projects" node in the database
+const projectsRef = database.ref('projects');
+
+// Function to handle voting for a project
+function handleVote(projectId) {
+    const projectRef = projectsRef.child(projectId);
+    
+    projectRef.transaction(function(currentData) {
+        // Increment the vote count
+        return {
+            ...currentData,
+            voteCount: (currentData.voteCount || 0) + 1
+        };
+    });
+}
+
+// Function to update vote counts in the UI
+function updateVoteCounts() {
+    projectsRef.on('value', function(snapshot) {
+        const projects = snapshot.val();
+        
+        for (const projectId in projects) {
+            const voteCount = projects[projectId].voteCount || 0;
+            const voteCountElement = document.getElementById(`${projectId}-count`);
+            if (voteCountElement) {
+                voteCountElement.textContent = `${projects[projectId
